8effb56ec5e05cacf6d359907baf7fc8460a0950
Sahil Kumar <145154306+sahilmukhii@users.noreply.github.com>
2025-06-23 14:14:13 +0530

    Delete sql-project.sql

--- sql-project.sql
+++
@@ -1,8 +0,0 @@
--- Calculate the total revenue generated from pizza sales.
-
-SELECT 
-    SUM(orders_details.quantity * pizzas.price) AS total_sales
-FROM
-    orders_details
-        JOIN
-    pizzas ON pizzas.pizza_id = orders_details.pizza_id;
